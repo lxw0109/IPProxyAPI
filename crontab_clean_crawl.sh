1c4efac5f970cd38475b4a734372f5f8a6607e99
lxw.ucas <lxw.ucas@gmail.com>
2017-05-03 14:37:35 +0800

    create new branch: online

--- crontab_clean_crawl.sh
+++ crontab_clean_crawl.sh
@@ -1,4 +1,5 @@
-cd /home/lxw/IT/projects/IPProxyAPI
+# cd /home/lxw/IT/projects/IPProxyAPI
+cd /usr/share/nginx/html/IPProxyAPI
 source py3flask/bin/activate
 cd /home/lxw/IT/projects/IPProxyAPI/redis_IP_proxy/get_proxies
 python proxies_spider.py
