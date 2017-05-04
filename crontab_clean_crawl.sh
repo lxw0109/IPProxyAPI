c2f1cba92cc1fa84bce8edda69af3e842553e7ed
lxw.ucas <lxw.ucas@gmail.com>
2017-05-04 14:02:55 +0800

    update crontab_clean_crawl.sh

--- crontab_clean_crawl.sh
+++ crontab_clean_crawl.sh
@@ -1,8 +1,10 @@
+# source /home/jason/.bashrc
 # cd /home/lxw/IT/projects/IPProxyAPI
-cd /usr/share/nginx/html/IPProxyAPI
+# cd /usr/share/nginx/html/IPProxyAPI
 # source py3flask/bin/activate
-workon py3flask
 # cd /home/lxw/IT/projects/IPProxyAPI/redis_IP_proxy/get_proxies
+
 cd /usr/share/nginx/html/IPProxyAPI/redis_IP_proxy/get_proxies
+workon py3flask
 python proxies_spider.py
 
